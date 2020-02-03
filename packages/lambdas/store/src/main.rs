98766a41f248f441507f92c0218790e5f5389a0d
Matt Brown <matt@ederoyd.co.uk>
2020-02-03 21:01:30 +0000

    Added Rust AWS SDK

--- packages/lambdas/store/src/main.rs
+++ packages/lambdas/store/src/main.rs
@@ -1,10 +1,12 @@
 use std::error::Error;
-
 use lambda_runtime::{error::HandlerError, lambda, Context};
-use log::{self, error};
+use log::{self, error, info};
+use rusoto_core::Region;
+use rusoto_dynamodb::{AttributeValue, DynamoDb, DynamoDbClient, UpdateItemInput};
 use serde_derive::{Deserialize, Serialize};
 use simple_error::bail;
 use simple_logger;
+use std::collections::HashMap;
 
 #[derive(Deserialize)]
 struct CustomEvent {
@@ -18,9 +20,8 @@ struct CustomOutput {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    simple_logger::init_with_level(log::Level::Debug)?;
+    simple_logger::init_with_level(log::Level::Info)?;
     lambda!(my_handler);
-
     Ok(())
 }
 
@@ -30,7 +31,36 @@ fn my_handler(e: CustomEvent, c: Context) -> Result<CustomOutput, HandlerError> 
         bail!("Empty first name");
     }
 
+    let client = DynamoDbClient::new(Region::EuCentral1);
+
+    let item = create_item(&e.first_name);
+    let update_item = UpdateItemInput {
+        key: item.clone(),
+        table_name: "rust-serverless-store-dev".to_string(),
+        ..Default::default()
+    };
+
+    let item_from_dynamo = match client.update_item(update_item).sync() {
+        Ok(item) => item,
+        Err(e) => panic!("Error completing futures: {}", e),
+    };
+
+    info!("item: {:?}", item_from_dynamo);
+
     Ok(CustomOutput {
         message: format!("Hello, {}!", e.first_name),
     })
+}
+
+fn create_item(first_name: &String) -> HashMap<String, AttributeValue> {
+    let item_key = "firstName";
+    let mut item = HashMap::new();
+    item.insert(
+        item_key.to_string(),
+        AttributeValue {
+            s: Some(first_name.to_string()),
+            ..Default::default()
+        },
+    );
+    item
 }
