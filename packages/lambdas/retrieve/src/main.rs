813612754ee714310c20c584759008e2fb419ee0
Matthew Brown <matt@ederoyd.co.uk>
2020-08-31 08:40:03 +0200

    updates

--- packages/lambdas/retrieve/src/main.rs
+++ packages/lambdas/retrieve/src/main.rs
@@ -1,10 +1,11 @@
 use std::error::Error;
 
 use lambda_runtime::{error::HandlerError, lambda, Context};
-use log::{self, error};
+use log::{self, info};
 use serde_derive::{Deserialize, Serialize};
-use simple_error::bail;
 use simple_logger;
+use rusoto_core::{Region};
+use rusoto_s3::{S3, S3Client, PutObjectRequest};
 
 #[derive(Deserialize)]
 struct CustomEvent {
@@ -18,19 +19,27 @@ struct CustomOutput {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    simple_logger::init_with_level(log::Level::Debug)?;
+    simple_logger::init_with_level(log::Level::Info)?;
     lambda!(my_handler);
 
     Ok(())
 }
 
-fn my_handler(e: CustomEvent, c: Context) -> Result<CustomOutput, HandlerError> {
-    if e.first_name == "" {
-        error!("Empty first name in request {}", c.aws_request_id);
-        bail!("Empty first name");
-    }
+fn my_handler(e: CustomEvent, _c: Context) -> Result<CustomOutput, HandlerError> {
+    let s3_client = S3Client::new(Region::EuCentral1);
+    let result = match s3_client.put_object(PutObjectRequest {
+        bucket: String::from("bucket"),
+        key: e.first_name.clone(),
+        body: Some(e.first_name.clone().into_bytes().into()),
+        ..Default::default()
+    }).sync() {
+        Ok(item) => item,
+        Err(e) => panic!("Error completing futures: {}", e),
+    };
+
+    info!("item: {:?}", result);
 
     Ok(CustomOutput {
-        message: format!("Hello, {}!", e.first_name),
+        message: format!("Lambda completed for {}", e.first_name),
     })
 }
