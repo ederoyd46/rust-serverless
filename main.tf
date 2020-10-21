5620c9814670a48a67523b5159694e2f7f164a1a
Matthew Brown <matt@ederoyd.co.uk>
2020-10-21 16:22:38 +0200

    Changed to use s3 bucket to store the terraform state

--- main.tf
+++ main.tf
@@ -1,5 +1,15 @@
 provider "aws" {
+  region = "eu-central-1"
+  version = "~> 3.9.0"
+}
+
+terraform {
+  backend "s3" {
+    key = "46/terraform"
+    encrypt = true
+    bucket = "ederoyd"
     region = "eu-central-1"
+  }
 }
 
 # Data Storage
@@ -96,8 +106,6 @@ resource aws_iam_role_policy cloudwatch_policy {
 }
 EOF
 }
-
-
 
 
 
