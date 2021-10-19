9cf3dfdf84ae5ff64e30835be888aed9b12255df
Matthew Brown <matt@ederoyd.co.uk>
2021-10-19 22:02:45 +0200

    package upgrade, minor lint refactor

--- packages/lambdas/src/lib.rs
+++ packages/lambdas/src/lib.rs
@@ -4,8 +4,10 @@ use log::{debug, error};
 use std::env;
 
 pub fn extract_key_from_request(event: Request) -> String {
-    let path: Vec<&str> = event.uri().path().rsplit('/').collect();
-    path.into_iter().next().unwrap().to_string()
+    match event.uri().path().rsplit_once('/') {
+        Some((_prefix, path_name)) => path_name.to_string(),
+        None => error_and_panic!("Could not find Path from the environment"),
+    }
 }
 
 pub fn get_table_name() -> String {
