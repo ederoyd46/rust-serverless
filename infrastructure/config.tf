1307fc051a85dd5aca37fbd7f43777c7386582cf
Matthew Brown <matt@ederoyd.co.uk>
2022-04-10 09:53:28 +0000

    added commented out localstack config

--- infrastructure/config.tf
+++ infrastructure/config.tf
@@ -1,5 +1,22 @@
 provider "aws" {
   region = var.region
+
+  # access_key                  = "fake"
+  # secret_key                  = "fake"
+  # skip_credentials_validation = true
+  # skip_metadata_api_check     = true
+  # skip_requesting_account_id  = true
+
+  # endpoints {
+  #   dynamodb       = "http://localhost:4566"
+  #   lambda         = "http://localhost:4566"
+  #   eventbridge    = "http://localhost:4566"
+  #   s3             = "http://localhost:4566"
+  #   cloudwatchlogs = "http://localhost:4566"
+  #   iam            = "http://localhost:4566"
+  #   apigateway     = "http://localhost:4566"
+  # }
+
 }
 
 terraform {
